4cf23564fb5f31cf21d83ffa8e472929afd8af1b
kinyeramos2021 <kinyeramo@gmail.com>
2021-07-05 15:15:01 +0300

    setting isLoading to false

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0859f4618e0cc3eb5d8e"; }
+/******/ 		__webpack_require__.h = function() { return "edbb3abfba1af77b7c9d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/ImageToggleOnScroll.js
+++ src/ImageToggleOnScroll.js
@@ -4,13 +4,18 @@ const ImageToggleOnScroll = ({primaryImg, secondaryImg}) => {
     
   const imageRef = useRef(null);
 
+  const [isLoading, setIsLoading] = useState(true);
+
   const isInview = () => {
       const rect = imageRef.current.getBoundingClientRect();
       return rect.top >= 0 && rect.bottom <= window.innerHeight;
   };
 
   const [inView, setInView] = useState(false);
+
   useEffect(() => {
+      setIsLoading(false);
+      setInView(isInview());
    window.addEventListener('scroll', scrollHandler);
    return () => {
        window.removeEventListener("scroll", scrollHandler);
@@ -21,10 +26,14 @@ const ImageToggleOnScroll = ({primaryImg, secondaryImg}) => {
        setInView(isInview());
    };
 
-
+   
   return (
-<img src={inView ? secondaryImg : primaryImg} 
-        alt="" ref={imageRef} 
+<img src={
+    isLoading ? 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==' // 1x1gif
+    :inView ? secondaryImg : primaryImg} 
+        alt="" ref={imageRef}
+        width="200"
+        height="200" 
         />
   );
 };
